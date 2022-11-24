4f4d6d5faeaaf4ff4bf17ba64b27069687da64cd
Zaeem <syedzae123@gamil.com>
2022-11-24 09:38:25 -0800

    api generated scantum initialize

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Repositories\Interfaces\CategoryRepositoryInterface;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -17,4 +19,22 @@ class UserController extends Controller
         $users = $this->categoryRepository->all();
         return($users);
     }
+    public function login(Request $request)
+    {
+        $user= User::where('email', $request->email)->first();
+        
+             $token = $user->createToken('my-app-token')->plainTextToken;
+        
+            $response = [
+                'user' => $user,
+                'token' => $token
+            ];
+        
+             return response($response, 201);
+    }
+   public function data()
+   {
+      $user = User::all();
+      return $user; 
+   }
 }
